1757c7ca0081a636bb52f2b121be4dd1e9d7006f
eric001 <chen_x@live.com>
2017-07-14 15:06:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,5 +8,7 @@ int main(int argc, char const *argv[])
     std::cout <<"add one line"<<std::endl;
     std::cout <<"this is on testmerge branch"<<std::endl;
     std::cout <<"this is on testmerge2 branch"<<std::endl;
+	std::cout <<"this is on testmerge2 branch"<<std::endl;
+	
 	return 0;
 }
