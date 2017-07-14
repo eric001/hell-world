81d6634699ed9da40ea53248edc5ed3d1242ebfd
XiangChen <Xiang.Chen@ygomi.com>
2017-07-14 14:00:47 +0800

    testmerge2

---
+++ testmerge2.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: testmerge2.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 14 Jul 2017 01:59:50 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+    return a+b;
+}
